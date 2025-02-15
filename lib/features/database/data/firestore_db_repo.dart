aef584344dbd2f2d67294632b171a7b7684adb43
Yahya56SR <168217051+Yahya56SR@users.noreply.github.com>
2025-02-16 00:23:16 +0100

    modified:   lib/features/database/data/firestore_db_repo.dart

--- lib/features/database/data/firestore_db_repo.dart
+++ lib/features/database/data/firestore_db_repo.dart
@@ -1,6 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:focus_planner/features/auth/domain/entities/app_user.dart';
 import 'package:focus_planner/features/database/domain/entities/timetable.dart';
+import 'package:focus_planner/features/database/domain/entities/task.dart';
+import 'package:focus_planner/features/database/domain/entities/holiday.dart';
+import 'package:focus_planner/features/database/domain/entities/exercice.dart';
+import 'package:focus_planner/features/database/domain/entities/connected_devices.dart';
+import 'package:focus_planner/features/database/domain/entities/school_level.dart';
 import 'package:focus_planner/features/database/domain/repo/db_repo.dart';
 
 class FirestoreDbRepo implements DbRepo {
@@ -25,156 +30,144 @@ class FirestoreDbRepo implements DbRepo {
       'subjectName': data.subjectName,
       'from': data.from,
       'to': data.to,
-      'userId': currentUser.uid,
-      'timestamp': FieldValue.serverTimestamp()
     });
   }
 
   @override
   Stream<QuerySnapshot> readTimetableData() {
-    final timetablesStream =
-        timetableData.orderBy('timestamp', descending: true).snapshots();
-    return timetablesStream;
+    return timetableData.orderBy('timestamp', descending: true).snapshots();
   }
 
   @override
-  Future<void> updateTimetableData(String docId, Timetable data) {
-    return timetableData.doc(docId).update({
-      'day': data.day,
-      'subjectName': data.subjectName,
-      'from': data.from,
-      'to': data.to,
-      'timestamp': FieldValue.serverTimestamp()
-    });
+  Future<Timetable> updateTimetableData(String? docId) async {
+    DocumentSnapshot doc = await timetableData.doc(docId).get();
+    return Timetable.fromSnapshot(doc);
   }
 
-  Future<void> deleteTimetableData(String docId) {
-    return timetableData.doc(docId).delete();
+  @override
+  Future<Timetable> deleteTimetableData(String? docId) async {
+    DocumentSnapshot doc = await timetableData.doc(docId).get();
+    await timetableData.doc(docId).delete();
+    return Timetable.fromSnapshot(doc);
   }
 
-  Future<void> createTask(Map<String, dynamic> taskData, AppUser currentUser) {
-    return this.taskData.add({
-      ...taskData,
-      'userId': currentUser.id,
-      'timestamp': FieldValue.serverTimestamp()
-    });
+  @override
+  Future<void> createTaskData(String? data) {
+    return taskData.add({'data': data});
   }
 
-  Stream<QuerySnapshot> readTasks(AppUser currentUser) {
-    return taskData
-        .where('userId', isEqualTo: currentUser.uid)
-        .orderBy('timestamp', descending: true)
-        .snapshots();
+  @override
+  Stream<QuerySnapshot> readTaskData() {
+    return taskData.snapshots();
   }
 
-  Future<void> updateTask(String docId, Map<String, dynamic> taskData) {
-    return this
-        .taskData
-        .doc(docId)
-        .update({...taskData, 'timestamp': FieldValue.serverTimestamp()});
+  @override
+  Future<Task> updateTaskData(String? docId) async {
+    DocumentSnapshot doc = await taskData.doc(docId).get();
+    return Task.fromSnapshot(doc);
   }
 
-  Future<void> deleteTask(String docId) {
-    return this.taskData.doc(docId).delete();
+  @override
+  Future<Task> deleteTaskData(String? docId) async {
+    DocumentSnapshot doc = await taskData.doc(docId).get();
+    await taskData.doc(docId).delete();
+    return Task.fromSnapshot(doc);
   }
 
-  Future<void> createHoliday(Map<String, dynamic> holidayData) {
-    return holidaysData
-        .add({...holidayData, 'timestamp': FieldValue.serverTimestamp()});
+  @override
+  Future<void> createHolidayData(String? data) {
+    return holidaysData.add({'data': data});
   }
 
-  Stream<QuerySnapshot> readHolidays() {
-    return holidaysData.orderBy('timestamp', descending: true).snapshots();
+  @override
+  Stream<QuerySnapshot> readHolidayData() {
+    return holidaysData.snapshots();
   }
 
-  Future<void> updateHoliday(String docId, Map<String, dynamic> holidayData) {
-    return holidaysData
-        .doc(docId)
-        .update({...holidayData, 'timestamp': FieldValue.serverTimestamp()});
+  @override
+  Future<Holiday> updateHolidayData(String? docId) async {
+    DocumentSnapshot doc = await holidaysData.doc(docId).get();
+    return Holiday.fromSnapshot(doc);
   }
 
-  Future<void> deleteHoliday(String docId) {
-    return holidaysData.doc(docId).delete();
+  @override
+  Future<Holiday> deleteHolidayData(String? docId) async {
+    DocumentSnapshot doc = await holidaysData.doc(docId).get();
+    await holidaysData.doc(docId).delete();
+    return Holiday.fromSnapshot(doc);
   }
 
-  Future<void> createExercice(Map<String, dynamic> exerciceData) {
-    return exercicesData
-        .add({...exerciceData, 'timestamp': FieldValue.serverTimestamp()});
+  @override
+  Future<void> createExerciseData(String? data) {
+    return exercicesData.add({'data': data});
   }
 
-  Stream<QuerySnapshot> readExercices() {
-    return exercicesData.orderBy('timestamp', descending: true).snapshots();
+  @override
+  Stream<QuerySnapshot> readExerciseData() {
+    return exercicesData.snapshots();
   }
 
-  Future<void> updateExercice(String docId, Map<String, dynamic> exerciceData) {
-    return exercicesData
-        .doc(docId)
-        .update({...exerciceData, 'timestamp': FieldValue.serverTimestamp()});
+  @override
+  Future<Exercice> updateExerciseData(String? docId) async {
+    DocumentSnapshot doc = await exercicesData.doc(docId).get();
+    return Exercice.fromSnapshot(doc);
   }
 
-  Future<void> deleteExercice(String docId) {
-    return exercicesData.doc(docId).delete();
+  @override
+  Future<Exercice> deleteExerciseData(String? docId) async {
+    DocumentSnapshot doc = await exercicesData.doc(docId).get();
+    await exercicesData.doc(docId).delete();
+    return Exercice.fromSnapshot(doc);
   }
 
-  Future<void> createSchoolLevel(Map<String, dynamic> schoolLevelData) {
-    return this
-        .schoolLevelData
-        .add({...schoolLevelData, 'timestamp': FieldValue.serverTimestamp()});
+  @override
+  Future<void> createConnectedDevicesData(String? data) {
+    return connectedDevicesData.add({'data': data});
   }
 
-  Stream<QuerySnapshot> readSchoolLevels() {
-    return schoolLevelData.orderBy('timestamp', descending: true).snapshots();
+  @override
+  Stream<QuerySnapshot> readConnectedDevicesData() {
+    return connectedDevicesData.snapshots();
   }
 
-  Future<void> updateSchoolLevel(
-      String docId, Map<String, dynamic> schoolLevelData) {
-    return this.schoolLevelData.doc(docId).update(
-        {...schoolLevelData, 'timestamp': FieldValue.serverTimestamp()});
+  @override
+  Future<ConnectedDevices> updateConnectedDevicesData(String? docId) async {
+    DocumentSnapshot doc = await connectedDevicesData.doc(docId).get();
+    return ConnectedDevices.fromSnapshot(doc);
   }
 
-  Future<void> deleteSchoolLevel(String docId) {
-    return this.schoolLevelData.doc(docId).delete();
+  @override
+  Future<ConnectedDevices> deleteConnectedDevicesData(String? docId) async {
+    DocumentSnapshot doc = await connectedDevicesData.doc(docId).get();
+    await connectedDevicesData.doc(docId).delete();
+    return ConnectedDevices.fromSnapshot(doc);
   }
 
-  Future<void> createChallenge(Map<String, dynamic> challengeData) {
-    return challengesData
-        .add({...challengeData, 'timestamp': FieldValue.serverTimestamp()});
+  @override
+  Future<void> createSchoolLevelData(String? data) {
+    return schoolLevelData.add({'data': data});
   }
 
-  Stream<QuerySnapshot> readChallenges() {
-    return challengesData.orderBy('timestamp', descending: true).snapshots();
+  @override
+  Stream<QuerySnapshot> readSchoolLevelData() {
+    return schoolLevelData.snapshots();
   }
 
-  Future<void> updateChallenge(
-      String docId, Map<String, dynamic> challengeData) {
-    return challengesData
-        .doc(docId)
-        .update({...challengeData, 'timestamp': FieldValue.serverTimestamp()});
+  @override
+  Future<SchoolLevel> updateSchoolLevelData(String? docId) async {
+    DocumentSnapshot doc = await schoolLevelData.doc(docId).get();
+    return SchoolLevel.fromSnapshot(doc);
   }
 
-  Future<void> deleteChallenge(String docId) {
-    return challengesData.doc(docId).delete();
+  @override
+  Future<SchoolLevel> deleteSchoolLevelData(String? docId) async {
+    DocumentSnapshot doc = await schoolLevelData.doc(docId).get();
+    await schoolLevelData.doc(docId).delete();
+    return SchoolLevel.fromSnapshot(doc);
   }
 
-  Future<void> createConnectedDevice(Map<String, dynamic> deviceData) {
-    return connectedDevicesData
-        .add({...deviceData, 'timestamp': FieldValue.serverTimestamp()});
-  }
-
-  Stream<QuerySnapshot> readConnectedDevices() {
-    return connectedDevicesData
-        .orderBy('timestamp', descending: true)
-        .snapshots();
-  }
-
-  Future<void> updateConnectedDevice(
-      String docId, Map<String, dynamic> deviceData) {
-    return connectedDevicesData
-        .doc(docId)
-        .update({...deviceData, 'timestamp': FieldValue.serverTimestamp()});
-  }
-
-  Future<void> deleteConnectedDevice(String docId) {
-    return connectedDevicesData.doc(docId).delete();
+  @override
+  Stream<QuerySnapshot> readChallengeData() {
+    return challengesData.snapshots();
   }
 }
