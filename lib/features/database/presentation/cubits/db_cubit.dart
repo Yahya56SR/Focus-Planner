331c4175c603e4efc7ce171ded0e698d29dbc3b1
Yahya56SR <168217051+Yahya56SR@users.noreply.github.com>
2025-02-20 20:00:02 +0100

    modified:   lib/features/database/presentation/cubits/db_cubit.dart

---
+++ lib/features/database/presentation/cubits/db_cubit.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:focus_planner/features/database/presentation/cubits/db_states.dart';
+
+class DbCubit extends Cubit<DbStates> {
+  DbCubit(super.initialState);
+}
